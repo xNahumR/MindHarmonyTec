e098048a05e6ec8bc9cd2c8bb7c2b07ab27bcb99
NahumR <xnahumtec@gmail.com>
2024-04-11 21:35:29 -0600

    cambio de nombre del sitio

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - ModeVue',
+    titleTemplate: '%s - MindHarmony',
     title: 'Home',
     htmlAttrs: {
       lang: 'en',
